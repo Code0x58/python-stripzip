557310d535dcc86ea972bb1ba35b73218f0f6ed0
Oliver Bristow <obristow@mintel.com>
2018-09-07 14:33:39 -0500

    Add some details to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import codecs
+from os import path
+
 from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with codecs.open(path.join(here, "README.rst"), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="python-stripzip",
@@ -6,4 +16,8 @@ setup(
     setup_requires=["setuptools_scm", "wheel"],
     entry_points={"console_scripts": ["stripzip = stripzip:cli"]},
     py_modules=["stripzip"],
+    keywords='zipfile reproducible-builds',
+    url='https://github.com/Code0x58/python-stripzip/',
+    author="Oliver Bristow",
+    author_email='github+pypi@oliverbristow.co.uk',
 )
