1265731ea82c3975fb99b2e7a8cc7d3367cb4262
Oliver Bristow <obristow@mintel.com>
2018-09-08 20:07:15 -0500

    Add missing data to setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
-with codecs.open(path.join(here, "README.rst"), encoding='utf-8') as f:
+with codecs.open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name="python-stripzip",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm", "wheel"],
-    entry_points={"console_scripts": ["stripzip = stripzip:cli"]},
-    py_modules=["stripzip"],
-    keywords='zipfile reproducible-builds',
-    url='https://github.com/Code0x58/python-stripzip/',
     author="Oliver Bristow",
-    author_email='github+pypi@oliverbristow.co.uk',
+    author_email="github+pypi@oliverbristow.co.uk",
+    description="strip zip timestamps for reproducible builds",
+    entry_points={"console_scripts": ["stripzip = stripzip:cli"]},
+    keywords="zipfile reproducible-builds",
+    platforms=["any"],
+    license="MIT",
     long_description=long_description,
+    name="python-stripzip",
+    py_modules=["stripzip"],
+    setup_requires=["setuptools_scm", "wheel"],
+    url="https://github.com/Code0x58/python-stripzip/",
+    use_scm_version=True,
 )
