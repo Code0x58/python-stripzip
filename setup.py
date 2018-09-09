96352aa5f04241b30be3a475e96babf217810e53
Oliver Bristow <obristow@mintel.com>
2018-09-08 23:21:53 -0500

    Add packaging trove classifier

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         Development Status :: 5 - Production/Stable
         Intended Audience :: Developers
         Topic :: Software Development
+        Topic :: System :: Archiving :: Packaging
         License :: OSI Approved :: MIT License
         Programming Language :: Python :: 2
         Programming Language :: Python :: 2.7
